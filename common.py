f2fb86195b967ce3eddd2a044a9f8c445af8d903
Darren <guobaoyan01@163.com>
2022-06-11 17:54:11 +0800

    完成 positionwise FeedForward子层构建

--- common.py
+++ common.py
@@ -75,3 +75,29 @@ class MultiHeadAttention(nn.Module):
         del key
         del value
         return self.linear_list[-1](x)
+
+
+class LayerNorm(nn.Module):
+    def __init__(self, features, eps=1e-6):
+        super(LayerNorm, self).__init__()
+        # 这里之所以使用两组参数，是因为强制的将隐藏层输入拉到标准正态分布会限制模型的表达能力
+        # 这样经过课学习参数的 scale 和 shift 可以恢复其表达能力
+        self.a_2 = nn.Parameter(torch.ones(features))
+        self.b_2 = nn.Parameter(torch.zeros(features))
+        self.eps = eps
+
+    def forward(self, x):
+        mean = x.mean(-1, keepdim=True)
+        std = x.std(-1, keepdim=True)
+        return self.a_2 * (x - mean) / (std + self.eps) + self.b_2
+
+
+class PositionwiseFeedForward(nn.Module):
+    def __int__(self, d_model, d_ff, dropout=0.1):
+        super(PositionwiseFeedForward, self).__int__()
+        self.w_1 = nn.Linear(d_model, d_ff)
+        self.w_2 = nn.Linear(d_ff, d_model)
+        self.dropout = nn.Dropout(p=dropout)
+
+    def forward(self, x):
+        return self.w_2(self.dropout(self.w_1(x).relu()))
--- utils.py
+++ utils.py
@@ -14,16 +14,4 @@ def clones(module, N):
     return nn.ModuleList([copy.deepcopy(module) for _ in range(N)])
 
 
-class LayerNorm(nn.Module):
-    def __init__(self, features, eps=1e-6):
-        super(LayerNorm, self).__init__()
-        # 这里之所以使用两组参数，是因为强制的将隐藏层输入拉到标准正态分布会限制模型的表达能力
-        # 这样经过课学习参数的 scale 和 shift 可以恢复其表达能力
-        self.a_2 = nn.Parameter(torch.ones(features))
-        self.b_2 = nn.Parameter(torch.zeros(features))
-        self.eps = eps
 
-    def forward(self, x):
-        mean = x.mean(-1, keepdim=True)
-        std = x.std(-1, keepdim=True)
-        return self.a_2 * (x - mean) / (std + self.eps) + self.b_2
